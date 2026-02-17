d721019d35a3cc7559c25ab96150ee07431e141f
PoultryPants <nicomcc24@gmail.com>
2026-02-16 16:53:08 -0800

    Fixed Vite & github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://mmilanta.github.io/cube-sphere/",
+  base: "/dome-generator/",
 });
